4a24b6ed8ae27036292af9da9d41ad5832551b8a
unknown <quythang1997@gmail.com>
2019-12-22 13:24:04 +0700

    Fix cors 3

--- server/app.js
+++ server/app.js
@@ -9,18 +9,12 @@ const recordRouter = require("./src/routers/record");
 const commentRouter = require("./src/routers/comment");
 
 const app = express();
-app.use(cors)
+app.use(cors    )
 const PORT = process.env.PORT || 5000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json({}));
 
-app.all("/*", function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
-  res.header("Access-Control-Allow-Headers", "Content-Type");
-  next();
-});
 app.use(userRouter);
 app.use(lessonRouter);
 app.use(examRouter);
