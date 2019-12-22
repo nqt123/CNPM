5f1e527faf4bf2de998fc2c12279893951391eb7
unknown <quythang1997@gmail.com>
2019-12-22 13:20:02 +0700

    Fixed Cors

--- server/app.js
+++ server/app.js
@@ -1,29 +1,33 @@
-const express = require('express')
-require('./src/db/mongoose')
+const express = require("express");
+require("./src/db/mongoose");
 
+const userRouter = require("./src/routers/user");
+const lessonRouter = require("./src/routers/lesson");
+const examRouter = require("./src/routers/exam");
+const excerciseRouter = require("./src/routers/exercise");
+const recordRouter = require("./src/routers/record");
+const commentRouter = require("./src/routers/comment");
 
-const userRouter = require('./src/routers/user')
-const lessonRouter = require('./src/routers/lesson')
-const examRouter = require('./src/routers/exam')
-const excerciseRouter = require('./src/routers/exercise')
-const recordRouter = require('./src/routers/record')
-const commentRouter = require('./src/routers/comment')
+const app = express();
 
-const app = express()
+const PORT = process.env.PORT || 5000;
 
-const PORT = process.env.PORT || 5000
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json({}));
 
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json({}))
-
-app.use(userRouter)
-app.use(lessonRouter)
-app.use(examRouter)
-app.use(excerciseRouter)
-app.use(recordRouter)
-app.use(commentRouter)
-
+app.all("/*", function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+app.use(userRouter);
+app.use(lessonRouter);
+app.use(examRouter);
+app.use(excerciseRouter);
+app.use(recordRouter);
+app.use(commentRouter);
 
 app.listen(PORT, () => {
-    console.log("Server is Started")
-})
+  console.log("Server is Started");
+});
