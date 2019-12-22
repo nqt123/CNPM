6515ae2e98919edbd6536abce3a37137443ca46f
unknown <quythang1997@gmail.com>
2019-12-22 13:23:44 +0700

    Fix cors 2

--- server/app.js
+++ server/app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 require("./src/db/mongoose");
-
+const cors = require('cors')
 const userRouter = require("./src/routers/user");
 const lessonRouter = require("./src/routers/lesson");
 const examRouter = require("./src/routers/exam");
@@ -9,7 +9,7 @@ const recordRouter = require("./src/routers/record");
 const commentRouter = require("./src/routers/comment");
 
 const app = express();
-
+app.use(cors)
 const PORT = process.env.PORT || 5000;
 
 app.use(express.urlencoded({ extended: true }));
